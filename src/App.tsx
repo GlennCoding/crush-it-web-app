ac70c7ff97438cf9cad2fd2274bacbeaccc298d4
Glenn Gregor <45522270+GlennCoding@users.noreply.github.com>
2021-04-13 17:12:22 +0200

    Imported functions

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
 import React from "react";
+import HomePage from "./pages/home_page/HomePage";
+import LandingPage from "./pages/landing_page/LandingPage";
+import SignupPage from "./pages/signup_page/SignupPage";
+import LoginPage from "./pages/login_page/LoginPage";
 
 export default function App() {
-  return <div>App</div>;
+    return (
+        <div>
+            {/* <HomePage /> */}
+            {/* <LandingPage /> */}
+            {/* <SignupPage /> */}
+            <LoginPage />
+        </div>
+    );
 }
