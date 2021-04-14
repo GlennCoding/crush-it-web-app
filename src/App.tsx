39c56304219fbd154ced8d8a4ff806ff90dbccaa
Giorgi Sharashenidze <giorgi.sharashenidze@code.berlin>
2021-04-14 09:52:38 +0200

    Init

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-export default function App() {
-  return <div>App</div>;
-}
+const app = () => {
+  //define global state...
+  return <div></div>;
+};
+export default app;
