1fedecba79d4027b796ad535d51b40397cfa34f3
Glenn Gregor <45522270+GlennCoding@users.noreply.github.com>
2021-04-14 10:34:34 +0200

    Changes

--- src/pages/login_page/LoginPage.tsx
+++ src/pages/login_page/LoginPage.tsx
@@ -2,29 +2,26 @@ import React from "react";
 import styles from "./LoginPage.module.scss";
 
 export default function LoginPage() {
-  return (
-    <div className={styles.loginPage}>
-      <button>Back</button>
-      <h1>Login</h1>
-      <p>
-        New here? <a href="#">Create an account</a>.
-      </p>
-      <form action="#" method="post">
-        <label>Email</label>
-        <input type="email" name="email" id="email" />
-        <br />
-
-        <label>Password</label>
-        <input type="password" name="password" id="password" />
-
-        <div className="rememberWrapper">
-          <label>Remember this device</label>
-          <input type="checkbox" name="remember" id="remember" />
+    console.log("Git Test");
+    return (
+        <div className={styles.loginPage}>
+            <button>Back</button>
+            <h1>Login</h1>
+            <p>
+                New here? <a href="#">Create an account</a>.
+            </p>
+            <form action="POST">
+                <label>Email</label>
+                <input type="email" name="email" id="email" />
+                <label>Password</label>
+                <input type="password" name="password" id="password" />
+                <div className="rememberWrapper">
+                    <label>Remember this device</label>
+                    <input type="checkbox" name="remember" id="remember" />
+                </div>
+                <input type="submit" value="Login" />
+            </form>
+            <button className="signupButton">Login with Google</button>
         </div>
-
-        <input type="submit" value="Login" />
-      </form>
-      <button className="signupButton">Login with Google</button>
-    </div>
-  );
+    );
 }
