5037a65435b12dc381a040c70d6a8d8ff72835a1
Giorgi Sharashenidze <giorgi.sharashenidze@code.berlin>
2021-04-15 07:01:06 +0200

    modified

--- src/pages/login_page/LoginPage.tsx
+++ src/pages/login_page/LoginPage.tsx
@@ -2,27 +2,27 @@ import React from "react";
 import styles from "./LoginPage.module.scss";
 
 export default function LoginPage() {
-    //Comment
-    return (
-        <div className={styles.loginPage}>
-            <button>Back</button>
-            <h1>Login</h1>
-            <p>
-                New here? <a href="#">Create an account</a>.
-            </p>
-            <form action="#" method="post">
-                <label>Email</label>
-                <input type="email" name="email" id="email" />
-                <br />
+  
+  return (
+    <div className={styles.loginPage}>
+      <button>Back</button>
+      <h1>Login</h1>
+      <p>
+        New here? <a href="#">Create an account</a>.
+      </p>
+      <form action="#" method="post">
+        <label>Email</label>
+        <input type="email" name="email" id="email" />
+        <br />
 
-                <label>Password</label>
-                <input type="password" name="password" id="password" />
+        <label>Password</label>
+        <input type="password" name="password" id="password" />
 
-                <div className="rememberWrapper">
-                    <label>Remember this device</label>
-                    <input type="checkbox" name="remember" id="remember" />
-                </div>
-            </form>
+        <div className="rememberWrapper">
+          <label>Remember this device</label>
+          <input type="checkbox" name="remember" id="remember" />
         </div>
-    );
+      </form>
+    </div>
+  );
 }
