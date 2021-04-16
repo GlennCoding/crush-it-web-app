c04dbd1f917fc294af09e550b345d87d69f13448
Giorgi Sharashenidze <giorgi.sharashenidze@code.berlin>
2021-04-16 08:05:45 +0200

    modified

--- src/pages/landing_page/LandingPage.tsx
+++ src/pages/landing_page/LandingPage.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styles from "./LandingPage.module.scss";
 import Logo from "../../images/crush_it_logo/crush-it-logo-black-normal.png";
+import { Link } from "react-router-dom";
 
 export default function LandingPage() {
   return (
@@ -9,8 +10,12 @@ export default function LandingPage() {
         <img src={Logo} alt="Crush It Logo" className={styles.logo} />
       </div>
       <div className={styles.buttonWrapper}>
-        <a className={styles.loginButton}>Login</a>
-        <a className={styles.signupButton}>Sign Up</a>
+        <Link className={styles.loginButton} to="/login">
+          Login
+        </Link>
+        <Link className={styles.signupButton} to="/register">
+          Sign Up
+        </Link>
       </div>
     </div>
   );
