4b1088355682435190f4e44b91b97c2418a9850d
Giorgi Sharashenidze <giorgi.sharashenidze@code.berlin>
2021-04-16 07:15:26 +0200

    Basic React Router Dom setup

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,34 @@ import HomePage from "./pages/home_page/HomePage";
 import LandingPage from "./pages/landing_page/LandingPage";
 import SignupPage from "./pages/signup_page/SignupPage";
 import LoginPage from "./pages/login_page/LoginPage";
+import { BrowserRouter, Route, Link, Switch, Redirect } from "react-router-dom";
 
 export default function App() {
   return (
     <div>
-      {/* <HomePage /> */}
-      <LandingPage />
-      {/* <SignupPage /> */}
-      {/* <LoginPage /> */}
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/home">
+            <HomePage />
+          </Route>
+          <Route exact path="/register">
+            <SignupPage />
+          </Route>
+          <Route exact path="/login">
+            <LoginPage />
+          </Route>
+          <Route exact path="/landing">
+            <LandingPage />
+          </Route>
+          <Route
+            exact
+            path="/"
+            render={() => {
+              return false ? <Redirect to="/home" /> : <Redirect to="/login" />;
+            }}
+          />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
--- src/pages/landing_page/LandingPage.tsx
+++ src/pages/landing_page/LandingPage.tsx
@@ -3,15 +3,15 @@ import styles from "./LandingPage.module.scss";
 import Logo from "../../images/crush_it_logo/crush-it-logo-black-normal.png";
 
 export default function LandingPage() {
-    return (
-        <div className={styles.landingPage}>
-            <div className={styles.logoWrapper}>
-                <img src={Logo} alt="Crush It Logo" className={styles.logo} />
-            </div>
-            <div className={styles.buttonWrapper}>
-                <a className={styles.loginButton}>Login</a>
-                <a className={styles.signupButton}>Sign Up</a>
-            </div>
-        </div>
-    );
+  return (
+    <div className={styles.landingPage}>
+      <div className={styles.logoWrapper}>
+        <img src={Logo} alt="Crush It Logo" className={styles.logo} />
+      </div>
+      <div className={styles.buttonWrapper}>
+        <a className={styles.loginButton}>Login</a>
+        <a className={styles.signupButton}>Sign Up</a>
+      </div>
+    </div>
+  );
 }
