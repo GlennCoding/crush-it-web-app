981a5aafe98b30eb00863859ef69249c416ab6ef
Giorgi Sharashenidze <giorgi.sharashenidze@code.berlin>
2021-04-19 09:10:46 +0200

    Submitting form functionality completed

--- src/pages/signup_page/SignupPage.tsx
+++ src/pages/signup_page/SignupPage.tsx
@@ -2,32 +2,55 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import styles from "./SignUpPage.module.scss";
 export default function SignupPage() {
+  const [name, setName] = useState<string>("");
+
   const [email, setEmail] = useState<string>("");
   const [password, setPassword] = useState<string>("");
 
   const onFormSubmitted = (event: any) => {
     event.preventDefault();
-    console.log(email, password);
+    console.log(email, password, name);
   };
   return (
     <div className="signupPage">
       <Link className={styles.backButton} to="/landing">
         Back
       </Link>
+
       <h1>Create Account</h1>
-      <form>
+
+      <form onSubmit={onFormSubmitted}>
         <label className="formLabel">Your Name</label>
-        <input type="text" name="name" id="name" />
+        <input
+          onChange={(e) => setName(e.target.value)}
+          value={name}
+          type="text"
+          id="name"
+        />
+
         <label className="formLabel">Your Email</label>
-        <input type="email" name="email" id="email" />
+        <input
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+          type="email"
+          id="email"
+        />
+
         <label className="formLabel">Your Password</label>
-        <input type="password" name="password" id="password" />
+        <input
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+          type="password"
+          id="password"
+        />
+
         <div className="agreementWrapper">
           <label className="formLabelSmall">
             I agree to the Terms &amp; Conditions and Privacy Policy
           </label>
           <input type="checkbox" name="agreement" id="agreement" />
         </div>
+
         <input type="submit" value="Create account" />
       </form>
       <button className="signupButton">Sign up with Google</button>
