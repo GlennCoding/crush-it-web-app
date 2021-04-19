f54b88e8c5a0e3f1e338ff9b88f0016f109eaacb
Giorgi Sharashenidze <giorgi.sharashenidze@code.berlin>
2021-04-19 14:25:00 +0200

    giorgi's Header

--- test.tsx
+++ test.tsx
@@ -1,14 +1,15 @@
 import React from "react";
 export default function LoginPage() {
-    //New commit
-    return (
-        <div>
-            <button className="className">Giorgi's button</button>
-            <p>Giorgi</p>
-            <p className="Glenn">Glenn</p>
-            <button>Glenn's buttton</button>
-        </div>
-    );
+  //New commit
+  return (
+    <div>
+      <button className="className">Giorgi's button</button>
+      <p>Giorgi</p>
+      <p className="Glenn">Glenn</p>
+      <button>Glenn's buttton</button>
+      <h1>Giorgi's Header</h1>
+    </div>
+  );
 }
 
 var li = [1, 2, 3, 4];
