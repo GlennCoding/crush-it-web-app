5244dbe7be7a045291bf22ee099630b7124ec505
Glenn Gregor <45522270+GlennCoding@users.noreply.github.com>
2021-04-19 15:29:43 +0200

    updated design

--- src/pages/signup_page/SignupPage.tsx
+++ src/pages/signup_page/SignupPage.tsx
@@ -5,80 +5,105 @@ import * as authServices from "../../services/auth_services";
 import axios from "axios";
 
 export default function SignupPage(props: {
-  setToken: (token: string) => void;
+    setToken: (token: string) => void;
 }) {
-  const [name, setName] = useState<string>("");
-  const [email, setEmail] = useState<string>("");
-  const [password, setPassword] = useState<string>("");
-  const [errorMessage, setErrorMessage] = useState<string>("");
+    const [name, setName] = useState<string>("");
+    const [email, setEmail] = useState<string>("");
+    const [password, setPassword] = useState<string>("");
+    const [errorMessage, setErrorMessage] = useState<string>("");
 
-  useEffect(() => {});
+    useEffect(() => {});
 
-  const onFormSubmitted = async (event: any) => {
-    event.preventDefault();
-    console.log(email, password, name);
-    try {
-      const response = await authServices.register({ email, name, password });
-      if (response.success) {
-        props.setToken(response.token);
-      } else {
-        setErrorMessage(response.message);
-      }
-    } catch {
-      setErrorMessage("Ups, please contact the team");
-    }
-  };
+    const onFormSubmitted = async (event: any) => {
+        event.preventDefault();
+        console.log(email, password, name);
+        try {
+            const response = await authServices.register({
+                email,
+                name,
+                password,
+            });
+            if (response.success) {
+                props.setToken(response.token);
+            } else {
+                setErrorMessage(response.message);
+            }
+        } catch {
+            setErrorMessage("Ups, please contact the team");
+        }
+    };
 
-  return (
-    <div className="signupPage">
-      <Link className={styles.backButton} to="/landing-page">
-        Back
-      </Link>
-      <h1>Create Account</h1>
-      <form onSubmit={onFormSubmitted}>
-        <label className="formLabel">Your Name</label>
-        <input
-          required
-          onChange={(e) => setName(e.target.value)}
-          value={name}
-          type="text"
-          id="name"
-        />
-        <br />
+    return (
+        <div className={styles.signUpPage}>
+            <Link className={styles.pageBackButton} to="/landing-page">
+                Back
+            </Link>
+            <h1 className={styles.pageHeader}>Create Account</h1>
+            <form className={styles.signUpForm} onSubmit={onFormSubmitted}>
+                <div className={styles.inputTextWrapper}>
+                    <label className="formLabel">Your Name</label>
+                    <input
+                        className={styles.inputTextField}
+                        required
+                        onChange={(e) => setName(e.target.value)}
+                        value={name}
+                        type="text"
+                        id="name"
+                    />
+                </div>
 
-        <label className="formLabel">Your Email</label>
-        <input
-          required
-          onChange={(e) => setEmail(e.target.value)}
-          value={email}
-          type="email"
-          id="email"
-        />
-        <br />
+                <div className={styles.inputTextWrapper}>
+                    <label className="formLabel">Your Email</label>
+                    <input
+                        className={styles.inputTextField}
+                        required
+                        onChange={(e) => setEmail(e.target.value)}
+                        value={email}
+                        type="email"
+                        id="email"
+                    />
+                </div>
 
-        <label className="formLabel">Your Password</label>
-        <input
-          required
-          onChange={(e) => setPassword(e.target.value)}
-          value={password}
-          type="password"
-          id="password"
-        />
+                <div className={styles.inputTextWrapper}>
+                    <label className="formLabel">Your Password</label>
+                    <input
+                        className={styles.inputTextField}
+                        required
+                        onChange={(e) => setPassword(e.target.value)}
+                        value={password}
+                        type="password"
+                        id="password"
+                    />
+                </div>
 
-        <div className="agreementWrapper">
-          <label className="formLabelSmall">
-            I agree to the Terms &amp; Conditions and Privacy Policy
-          </label>
-          <input type="checkbox" name="agreement" id="agreement" />
+                <div className={styles.inputCheckboxWrapper}>
+                    <label>
+                        I agree to the <a href="#">Terms &amp; Conditions</a>{" "}
+                        and <a href="#">Privacy Policy</a>
+                    </label>
+                    <input
+                        className={styles.inputCheckbox}
+                        type="checkbox"
+                        name="agreement"
+                        id="agreement"
+                    />
+                </div>
+
+                <input
+                    className={styles.signUpButton}
+                    type="submit"
+                    value="Create account"
+                />
+                <button className={styles.signUpButtonGoogle}>
+                    Sign up with Google
+                </button>
+            </form>
+
+            <p>
+                Already have an account? <Link to="/login">Log in</Link>.
+            </p>
+
+            <p style={{ color: "red" }}>{errorMessage}</p>
         </div>
-
-        <input type="submit" value="Create account" />
-      </form>
-      <button className="signupButton">Sign up with Google</button>
-      <p>
-        Already have an account? <a href="#">Log in</a>.
-      </p>
-      <p style={{ color: "red" }}>{errorMessage}</p>
-    </div>
-  );
+    );
 }
