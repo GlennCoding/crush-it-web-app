94ee8b7c493bf0c79738c9fbea0e64df0d4d8bec
Glenn Gregor <45522270+GlennCoding@users.noreply.github.com>
2021-04-19 14:22:22 +0200

    test 5

--- test.tsx
+++ test.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 export default function LoginPage() {
-  //New commit
-  return (
-    <div>
-      <button className="className">Giorgi's button</button>
-      <p>Giorgi</p>
-      <p className="Glenn">Glenn</p>
-    </div>
-  );
+    //New commit
+    return (
+        <div>
+            <button className="className">Giorgi's button</button>
+            <p>Giorgi</p>
+            <p className="Glenn">Glenn</p>
+            <button>Glenn's buttton</button>
+        </div>
+    );
 }
 
 var li = [1, 2, 3, 4];
