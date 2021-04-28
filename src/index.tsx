1cd94775793ae5f059e5ccd534c11524fb28339c
Glenn Gregor <45522270+GlennCoding@users.noreply.github.com>
2021-04-28 15:03:08 +0200

    changed sass styles folder name

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./sassStyles/global.scss";
-import "./sassStyles/typography.scss";
+import "./sass_styles/global.scss";
+import "./sass_styles/typography.scss";
 import App from "./App";
 import axios from "axios";
 
