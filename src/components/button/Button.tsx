9ffae88fe870a5f8b5fc90f7e90d7470825b59be
Glenn Gregor <45522270+GlennCoding@users.noreply.github.com>
2021-05-02 21:54:45 +0200

    Updated button component

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,30 +1,46 @@
+import { BorderAll } from "@material-ui/icons";
 import React from "react";
 import styles from "./Button.module.scss";
 
 interface ButtonProps {
     text?: string;
     size: "sm" | "md" | "lg";
-    type?: "button" | "submit" | "reset";
     color: "primary" | "white" | "dark3";
+    type?: "button" | "submit" | "reset";
     icon?: JSX.Element;
 }
 
 const Button = (props: ButtonProps) => {
+    let size;
+    let color;
+
+    switch (props.size) {
+        case "sm":
+            size = styles.sm;
+            break;
+        case "md":
+            size = styles.md;
+            break;
+        case "lg":
+            size = styles.lg;
+            break;
+    }
+
+    switch (props.color) {
+        case "white":
+            color = styles.white;
+            break;
+        case "primary":
+            color = styles.primary;
+            break;
+        case "dark3":
+            color = styles.dark3;
+            break;
+    }
+
     return (
         <button
-            className={`${styles.button} ${
-                props.size === "sm"
-                    ? styles.sm
-                    : props.size === "md"
-                    ? styles.md
-                    : styles.lg
-            } ${
-                props.color === "white"
-                    ? styles.white
-                    : props.color === "dark3"
-                    ? styles.dark3
-                    : styles.primary
-            }`}
+            className={`${styles.button} ${size} ${color}`}
             type={props.type}
         >
             {props.icon}
