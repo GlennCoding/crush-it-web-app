8b9e350e2263f07e762dd891505cb65544eaa13d
Glenn Gregor <45522270+GlennCoding@users.noreply.github.com>
2021-06-14 10:22:34 +0200

    refactored homepage

--- client/src/pages/landing_page/LandingPage.tsx
+++ client/src/pages/landing_page/LandingPage.tsx
@@ -4,19 +4,19 @@ import Logo from "../../images/crush_it_logo/crush-it-logo-black-normal.png";
 import { Link } from "react-router-dom";
 
 export default function LandingPage() {
-    return (
-        <div className={styles.landingPage}>
-            <div className={styles.logoWrapper}>
-                <img src={Logo} alt="Crush It Logo" className={styles.logo} />
-            </div>
-            <div className={styles.buttonWrapper}>
-                <Link className={styles.loginButton} to="/login">
-                    Login
-                </Link>
-                <Link className={styles.signupButton} to="/register">
-                    Sign Up
-                </Link>
-            </div>
-        </div>
-    );
+  return (
+    <div className={styles.landingPage}>
+      <div className={styles.logoWrapper}>
+        <img src={Logo} alt="Crush It Logo" className={styles.logo} />
+      </div>
+      <div className={styles.buttonWrapper}>
+        <Link className={styles.loginButton} to="/login">
+          Login
+        </Link>
+        <Link className={styles.signupButton} to="/register">
+          Sign Up
+        </Link>
+      </div>
+    </div>
+  );
 }
