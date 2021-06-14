afbf247733249fb2f28b913b73e49dc79ff5327d
gio-shara-code <74867549+gio-shara-code@users.noreply.github.com>
2021-06-14 09:42:53 +0200

    Delete src directory

--- src/components/error_message_text/ErrorMessageText.tsx
+++
@@ -1,8 +0,0 @@
-import React, { useState } from "react";
-import styles from "./ErrorMessageText.module.scss";
-
-const ErrorMessageText = (props: { errorMessage: string }) => {
-    return <p className={styles.errorMessageText}>{props.errorMessage}</p>;
-};
-
-export default ErrorMessageText;
--- src/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./sass_styles/global.scss";
-import "./sass_styles/typography.scss";
-import App from "./App";
-import axios from "axios";
-
-axios.defaults.baseURL = "https://api-crush-it.herokuapp.com";
-ReactDOM.render(<App />, document.getElementById("root"));
