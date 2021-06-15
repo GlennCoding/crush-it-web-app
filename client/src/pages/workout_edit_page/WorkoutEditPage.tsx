741e4d5a4af6dcdbb3b7b080305293ba3424f9d8
Giorgi Sharashenidze <giorgi.sharashenidze@code.berlin>
2021-06-15 13:38:48 +0200

    Have fun.

--- client/src/pages/workout_edit_page/WorkoutEditPage.tsx
+++ client/src/pages/workout_edit_page/WorkoutEditPage.tsx
@@ -1,23 +1,35 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./WorkoutEditPage.module.scss";
 import * as icons from "@material-ui/icons";
 import data from "./circuits";
 import CircuitList from "./circuit_list/CircuitList";
 import { Link } from "react-router-dom";
-
+import { Workout } from "../../interfaces/workout";
+import Loader from "../../components/loader/Loader";
 interface WorkoutEditPageProps {
-  workoutId: string | null;
-  name?: string;
-  description?: string;
+  workoutProps: Workout;
 }
 
-const WorkoutEditPage: React.FC<WorkoutEditPageProps> = ({
-  name,
-  description,
-  workoutId,
-}) => {
-  const componentDidMount = () => {};
+const WorkoutEditPage: React.FC<WorkoutEditPageProps> = ({ workoutProps }) => {
+  const [workout, setWorkout] = useState<Workout>(workoutProps);
+  const componentDidMount = () => {
+    if (!workout) {
+      //Create New Workout
+      // POST /workout (authorization token)
+      //Response -> workoutObject
+    } else {
+      //Get circutis based on the cirscuits ids
+      setWorkout(workout);
+      // GET /circuits () (body: workoutProps.circuitIds) (authorization token)
+      //Response -> curcuits[]
+    }
+  };
   useEffect(componentDidMount, []);
+
+  if (!workout) {
+    return <Loader />;
+  }
+
   return (
     <div className={styles.page}>
       {/* Navbar */}
@@ -26,12 +38,11 @@ const WorkoutEditPage: React.FC<WorkoutEditPageProps> = ({
           <Link to="/home">
             <icons.ChevronLeft className={styles.icon} />
           </Link>
-          <h3>{name}</h3>
+          <h3>Workout 1</h3>
           <icons.Tune className={styles.icon} />
         </div>
-        <p className={styles.description}>{description}</p>
+        <p className={styles.description}>Workout Description</p>
       </div>
-      {/* Circuit List */}
       <CircuitList data={data} />
     </div>
   );
