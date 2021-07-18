605d045e4bc84494a287e137eb9a1d120edd7a75
Giorgi Sharashenidze <giorgi.sharashenidze@code.berlin>
2021-07-18 15:14:58 +0200

    Added window favicon

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,47 +1,48 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-  mode: process.env.NODE_ENV,
-  entry: "./src",
-  output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "out"),
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(tsx|ts)$/,
-        exclude: /node_modules/,
-        loader: "babel-loader",
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: "asset/resource",
-      },
-      {
-        test: /\.s[ac]ss$/i,
-        use: [
-          // Creates `style` nodes from JS strings
-          "style-loader",
-          // Translates CSS into CommonJS
-          "css-loader",
-          // Compiles Sass to CSS
-          "sass-loader",
+    mode: process.env.NODE_ENV,
+    entry: './src',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'out'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(tsx|ts)$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    // Creates `style` nodes from JS strings
+                    'style-loader',
+                    // Translates CSS into CommonJS
+                    'css-loader',
+                    // Compiles Sass to CSS
+                    'sass-loader',
+                ],
+            },
         ],
-      },
+    },
+    devServer: {
+        port: 9001,
+        historyApiFallback: true,
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js', '.scss', '.sass'],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './public/index.html',
+            favicon: './public/favicon.ico',
+        }),
     ],
-  },
-  devServer: {
-    port: 9001,
-    historyApiFallback: true,
-  },
-  resolve: {
-    extensions: [".tsx", ".ts", ".js", ".scss", ".sass"],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./public/index.html",
-    }),
-  ],
-};
+}
